918e7f3c36648fc493d14412e0ebce3b517030ed
Your Name <daviih2000@gmail.com>
2025-03-12 07:11:13 -0300

    feat: add Prettier for code formatting and update README with project description

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório criado para estudar programação através da implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-  return <div><h1>Carta para meu bem querer:</h1>
-  <p>Lívia, eu amo você. Se você me ama, dá uma risadinha! 😎</p></div>
-
+  return (
+    <div>
+      <h1>Carta para meu bem querer:</h1>
+      <p>Lívia, eu amo você. Se você me ama, dá uma risadinha! 😎</p>
+    </div>
+  );
 }
 
 export default Home;
